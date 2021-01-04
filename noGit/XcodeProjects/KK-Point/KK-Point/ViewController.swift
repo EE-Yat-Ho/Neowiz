8ab4c465fd8e0a65fbb5c1de1778b46179986bbc
박영호 <EEYatHo@PYHiMac.local>
2021-01-04 17:36:07 +0900

    middle commit

---
+++ noGit/XcodeProjects/KK-Point/KK-Point/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KK-Point
+//
+//  Created by 박영호 on 2021/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
